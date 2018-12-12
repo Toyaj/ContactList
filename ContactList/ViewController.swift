1da4086afd38974d279c1772208930554b32f45f
toyaj.nigam <toyaj.nigam1209@gmail.com>
2018-12-12 12:34:56 +0530

    Initial Commit

---
+++ ContactList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactList
+//
+//  Created by Toyaj Nigam on 12/12/18.
+//  Copyright © 2018 Toyaj Nigam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
